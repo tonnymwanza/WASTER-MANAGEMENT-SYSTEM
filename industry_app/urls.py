2700fb7d95f94bba88d3a9f873191e1bd10256ca
tonnymwanza <tonnimwanza@gmail.com>
2024-06-04 06:58:06 +0300

    latest commits

--- industry_app/urls.py
+++ industry_app/urls.py
@@ -5,6 +5,7 @@ from . views import AboutView
 from . views import SolutionsView
 from . views import ContactView
 from . views import CollectorsView
+from . import views
 # my urls
 
 urlpatterns = [
@@ -13,4 +14,6 @@ urlpatterns = [
     path('solutions', SolutionsView.as_view(), name='solutions'),
     path('contact', ContactView.as_view(), name='contact'),
     path('collectors', CollectorsView.as_view(), name='collectors'),
+    path('signin', views.signin, name='signin'),
+    path('register', views.register, name='register'),
 ]
--- industry_app/views.py
+++ industry_app/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.views import View
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import auth
+from django.contrib.auth import authenticate
+from django.contrib import messages
 
 from . forms import RegisterForm
 from . forms import ContactForm
 from . models import Contact
+from . models import Register
 # Create your views here.
 
 class HomeView(View):
@@ -47,7 +52,7 @@ class ContactView(View):
             messages.error(request, 'problem sending info')
         return redirect('contact')
     
-class CollectorsView(View):
+class CollectorsView(LoginRequiredMixin, View):
 
     def get(self, request):
         form = RegisterForm()
@@ -55,3 +60,66 @@ class CollectorsView(View):
             'form': form
         }
         return render(request, 'collectors.html', context)
+    
+    def post(self, request):
+        form = RegisterForm(request.POST or None)
+        register_object = None
+        if form.is_valid():
+            register_object = Register.objects.create(
+                name = form.cleaned_data['name'],
+                waste_sources = form.cleaned_data['waste_sources'],
+                waste_collection = form.cleaned_data['waste_collection'],
+                transportation = form.cleaned_data['transportation'],
+                waste_separation = form.cleaned_data['waste_separation'],
+                waste_treatment = form.cleaned_data['waste_treatement'],
+                waste_disposal = form.cleaned_data['waste_disposal']
+            )
+            form = RegisterForm()
+            messages.info(request, 'Information submitted successfully')
+        else:
+            messages.error(request, 'Error sending information')
+        print(request.POST)
+        context = {
+            'form': form
+        }
+        context
+        return redirect('collectors')
+    
+def register(request): # user registration
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'the username is in user. try another one')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, 'the email is in use. find another one')
+                return redirect('user_register')
+            else:
+                user = User.objects.create_user(username = username, email = email, password = password)
+                return redirect('signin')
+        else:
+            messages.error(request, 'ensure the passwords are matching')
+            return redirect('register')
+    return render(request, 'register.html')
+
+def signin(request): # user login
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username = username, password = password)
+        if user is not None:
+            auth.login(request, user)
+            if 'next' in request.POST:
+                return redirect(request.POST['next'])
+            else:
+                return redirect('home')
+        else:
+            messages.error(request, 'invalid details provided. make sure the password or username is correct')
+            return redirect('signin')
+    return render(request, 'login.html')
